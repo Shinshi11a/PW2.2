3b4d2616a004a4fd0c0f871326d17fb56c6e8ae5
Shinshi11a <pro100vanyok2016@gmail.com>
2023-03-12 15:32:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class NumberManager
+{
+    private int n = 0;
+
+    public bool SetNumber(int number)
+    {
+        if (number == n + 1)
+        {
+            n = number;
+            return true;
+        }
+        else
+        {
+            n = 0;
+            return false;
+        }
+    }
+
+    public int GetExpectedNumber()
+    {
+        return n + 1;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        NumberManager numberManager = new NumberManager();
+
+        while (true)
+        {
+            Console.Write($"Введите число {numberManager.GetExpectedNumber()}: ");
+            int inputNumber;
+            if (int.TryParse(Console.ReadLine(), out inputNumber))
+            {
+                if (numberManager.SetNumber(inputNumber))
+                {
+                    Console.WriteLine("Верно!");
+                }
+                else
+                {
+                    Console.WriteLine("Неверно! Начинаем сначала.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода! Попробуйте еще раз.");
+            }
+        }
+    }
+}
